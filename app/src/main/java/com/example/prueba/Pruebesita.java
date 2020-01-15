ae3c48d3e23c50c3448a980b8b67f361e5aa346f
Makenrou <52251013+Makenrou@users.noreply.github.com>
2020-01-15 19:27:13 +0100

    Merge Adrian

---
+++ app/src/main/java/com/example/prueba/Pruebesita.java
@@ -0,0 +1,18 @@
+package com.example.prueba;
+
+public class Pruebesita {
+
+    String causas;
+
+    public Pruebesita(String causas) {
+        this.causas = causas;
+    }
+
+    public String getCausas() {
+        return causas;
+    }
+
+    public void setCausas(String causas) {
+        this.causas = causas;
+    }
+}
